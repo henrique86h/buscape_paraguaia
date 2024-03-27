b69593bfd8a289109d93fd8949e546a627e1fea4
henrique86h <henriqueribeiromiguel86@gmail.com>
2024-03-26 22:24:53 -0300

    fazendo a camisa polo

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buscapé Paraguaio</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body style="background-color: #e2eaef;">
+    <div class="container p-4">
+        <div class="row gx-5">
+            <div class="col-12 col-md-4 col-lg-3 mb-3 text-secondary">
+                <div class="p-3 bg-light shadow-sm text-center">
+                    <div class="text-end">
+                        <i class="bi-bi-heart"></i>
+                    </div>
+                    <img src="produtos/foto1.png" alt="Camisa Polo Masculina" class="img-fluid">
+                    <h4 class="fw-light">Camisa Polo Masculina</h4>
+                    <strong>Roupas</strong>
+                    <div class="row">
+                        <div class="col">
+                            <s class="fw-light">R$ 49,99</s>
+                        </div>
+                        <div class="col">
+                            <strong class="text-success h5">R$ 39,99</strong>
+                        </div>
+                    </div>
+                    <a href="#" class="btn btn-outline-sucess rounded-5  mt-4 fw-bold">Adicionar ao carrinho </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
